21914d9ac9caa8b7d4b2542ea37e70c245a26274
Josh_dfG <agboolajoshua161@gmail.com>
2026-02-18 20:12:49 +0100

    feat: add class work for day3

---
+++ day-3/contracts/ERC20.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+contract ERC20 {
+    // transfer
+    // transferfrom
+    // approve
+    // allowance
+    // balanceOf
+    // totalSupply
+    // name
+    // decimal
+    // symbol
+    // events
+    // Transfer()
+    // Approval
+    // Write a smart contract that can save both erc20 and ether for a
+    // user. Users must be able to:
+    // check individual balances,
+    // deposit or save in the contract.
+    // withdraw their savings
+
+    }
